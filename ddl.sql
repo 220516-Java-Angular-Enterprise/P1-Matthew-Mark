a7f4117bba01974dcdc6b00135457ec901a72ab2
mb_q <mbquercioli@gmail.com>
2022-06-05 20:33:32 -0600

    updated ddl.sql

--- ddl.sql
+++ ddl.sql
@@ -1,26 +1,26 @@
-drop table if exists ERS_REIMBURSEMENTS; 
-drop table if exists ERS_USERS;
-drop table if exists ERS_REIMBURSEMENT_STATUSES;
-drop table if exists ERS_REIMBURSEMENT_TYPES;
-drop table if exists ERS_USER_ROLES;
+drop table if exists ers_reimbursements; 
+drop table if exists ers_users;
+drop table if exists ers_reimbursement_statuses;
+drop table if exists ers_reimbursement_types;
+drop table if exists ers_user_roles;
 
 
 
-create table ERS_REIMBURSEMENTS (
-	REIMB_ID varchar,
-	AMOUNT int,
-	SUBMITTED TIMESTAMP, 
-	RESOLVED TIMESTAMP, 
-	DESCRIPTION varchar,
-	RECIEPT bytea,
-	PAYMENT_ID varchar,
-	AUTHOR_ID varchar,
-	RESOLVER_ID varchar,
-	STATUS_ID varchar,
-	TYPE_ID varchar,
+create table ers_reimbursements (
+	reimb_id varchar,
+	amount int,
+	submitted TIMESTAMP, 
+	resolved TIMESTAMP, 
+	description varchar,
+	receipt bytea,
+	payment_id varchar,
+	author_id varchar,
+	resolver_id varchar,
+	status_id varchar,
+	type_id varchar,
 	
-	constraint ERS_REIMBURSEMENTS
-		primary key (REIMB_ID)
+	constraint pk_ers_reimbursements
+		primary key (reimb_id)
 );	
 
 create table ERS_USERS(
@@ -33,7 +33,7 @@ create table ERS_USERS(
 	IS_ACTIVE boolean,
 	ROLE_ID varchar,
 	
-	constraint ERS_USERS
+	constraint PK_ERS_USERS
 		primary key (USER_ID)
 );
 
@@ -42,7 +42,7 @@ create table ERS_REIMBURSEMENT_STATUSES(
 	STATUS varchar,
 
 	
-	constraint ERS_REIMBURSEMENT_STATUSES
+	constraint PK_ERS_REIMBURSEMENT_STATUSES
 		primary key (STATUS_ID)
 );
 
@@ -50,7 +50,7 @@ create table ERS_REIMBURSEMENT_TYPES(
 	TYPE_ID varchar,
 	TYPE_NAME varchar,
 	
-	constraint ERS_REIMBURSEMENT_TYPES
+	constraint PK_ERS_REIMBURSEMENT_TYPES
 		primary key (TYPE_ID)
 );
 
@@ -58,22 +58,22 @@ create table ERS_USER_ROLES(
 	ROLE_ID varchar,
 	ROLE_NAME varchar,
 	
-	constraint ERS_USER_ROLES
+	constraint PK_ERS_USER_ROLES
 		primary key(ROLE_ID)
 );
 
-alter table ERS_REIMBURSEMENTS
+alter table ers_reimbursements 
 	add CONSTRAINT FK_AUTHOR_ID
-		foreign key (AUTHOR_ID) references ERS_USERS (USER_ID),
+		foreign key (author_id) references ERS_USERS (USER_ID),
 		
 	add CONSTRAINT FK_STATUS_ID
-		foreign key (STATUS_ID) references ERS_REIMBURSEMENT_STATUSES (STATUS_ID),
+		foreign key (status_id) references ERS_REIMBURSEMENT_STATUSES (STATUS_ID),
 		
 	add CONSTRAINT FK_REIMBURSEMENT_TYPES_ID
-		foreign key (TYPE_ID) references ERS_REIMBURSEMENT_TYPES (TYPE_ID),
+		foreign key (type_id) references ERS_REIMBURSEMENT_TYPES (TYPE_ID),
 	
 	add CONSTRAINT FK_RESOLVER_ID
-		foreign key (RESOLVER_ID) references ERS_USERS (USER_ID);
+		foreign key (resolver_id) references ERS_USERS (USER_ID);
 	
 alter table ERS_USERS	
 	add constraint FK_ROLE_ID
