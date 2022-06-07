f77d70d85ab67a2379ccc34cf763c948ce414c67
Matthew Berkheimer <MatthewBerkheimerWork@gmail.com>
2022-06-07 12:34:51 -0700

    Framework for UsersServices with validation methods

--- src/main/java/com/revature/shoe/services/UsersServices.java
+++ src/main/java/com/revature/shoe/services/UsersServices.java
@@ -1,4 +1,44 @@
 package com.revature.shoe.services;
 
+import com.revature.shoe.daos.UsersDAO;
+import com.revature.shoe.dtos.requests.NewUserRequest;
+import com.revature.shoe.models.Users;
+import com.revature.shoe.util.annotations.Inject;
+
+import java.util.UUID;
+
 public class UsersServices {
+    @Inject
+    private final UsersDAO usersDAO;
+
+    @Inject
+    public UsersServices(UsersDAO usersDAO){
+        this.usersDAO = usersDAO;
+    }
+
+    public Users register(NewUserRequest request){
+        //todo implement NewUserRequest
+        Users users = null;//request.extractUser();
+        if(isNotDuplicate(users.getUsername())){
+            if(isValidUsername(users.getUsername())){
+                if(isValidPassword(users.getPassword())){
+                    users.setUserID(UUID.randomUUID().toString());
+                    //usersDAO.save(users);
+                }
+            }
+        }
+        return users;
+    }
+
+    private boolean isValidUsername(String username){
+        return username.matches("^(?=[a-zA-Z0-9._]{8,20}$)(?!.*[_.]{2})[^_.].*[^_.]$");
+    }
+
+    private boolean isValidPassword(String password){
+        return password.matches("^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,}$");
+    }
+
+    private boolean isNotDuplicate(String username){
+        //return !usersDAO.InsertDaoHere.contains(username);
+    }
 }
