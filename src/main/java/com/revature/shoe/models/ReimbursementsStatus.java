5d119806d4998c006dda13d6f7877939f15fb153
Matthew Berkheimer <MatthewBerkheimerWork@gmail.com>
2022-06-07 12:12:50 -0700

    Created the models for P1

--- src/main/java/com/revature/shoe/models/ReimbursementsStatus.java
+++ src/main/java/com/revature/shoe/models/ReimbursementsStatus.java
@@ -1,4 +1,30 @@
 package com.revature.shoe.models;
 
 public class ReimbursementsStatus {
+    private String statusID;
+    private String status;
+
+    public ReimbursementsStatus() {
+    }
+
+    public ReimbursementsStatus(String statusID, String status) {
+        this.statusID = statusID;
+        this.status = status;
+    }
+
+    public String getStatusID() {
+        return statusID;
+    }
+
+    public void setStatusID(String statusID) {
+        this.statusID = statusID;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
